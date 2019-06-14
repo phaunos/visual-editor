0b56baca82fe1491af0fc3d55852f61b30513ee5
Javier Blanco García-Lomas <jblancogl@gmail.com>
2019-06-14 16:18:11 +0200

    added py-1 to button

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,13 @@
     <wavesurfer 
       ref="player"
       :source="source" />
-    <button 
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      @click="onPlay">
-      {{ playPauseText }}
-    </button>
+    <div class="container py-2">
+      <button 
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="onPlay">
+        {{ playPauseText }}
+      </button>
+    </div>
   </section>
 </template>
 
